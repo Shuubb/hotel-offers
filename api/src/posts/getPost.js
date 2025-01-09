94a81ea9e4ea38a22116347be8276dca6c53d5e4
Shuubb <shubitidzed9@gmail.com>
2025-01-09 17:51:13 +0400

    enhanced api

--- api/src/posts/getPost.js
+++ api/src/posts/getPost.js
@@ -1,11 +1,8 @@
 import findOne from '../database/findOne';
-import { ObjectId } from 'mongodb'; // If you're using the MongoDB Node.js driver
 
 export default async function (request, env) {
 	try {
-		const id = new ObjectId(request.id);
-
-		const result = await findOne('posts', { postId: id }, env);
+		const result = await findOne('posts', { postId: request.id }, env);
 		return new Response(JSON.stringify(result.document), { status: 200 });
 	} catch (error) {
 		console.error('Error retrieving posts:', error);
